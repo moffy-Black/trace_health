aa92f7014cfd56729aaf9618f85f1400029820ce
moffy-Black <black.river.emperor@outlook.jp>
2022-07-01 00:21:14 +0900

    [作成]FirebaseAuthログイン＆サインインパッケージ

---
+++ package/auth_service/lib/auth_service.dart
@@ -0,0 +1,4 @@
+library auth_service;
+
+export 'src/model/model.dart';
+export 'src/service/service.dart';
---
+++ package/auth_service/lib/src/service/auth.dart
@@ -0,0 +1,13 @@
+import '../model/model.dart';
+
+abstract class AuthService {
+  Future<UserEntity> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+
+  Future<UserEntity> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+}
