ae43f9d343738a1ec16969fcac1915cdf62128fe
moffy-Black <black.river.emperor@outlook.jp>
2022-07-03 23:56:12 +0900

    [作成]FireStoreに保存された体重と日付を線グラフで表示する機能

--- lib/home/view/weight_screen.dart
+++ lib/home/view/weight_screen.dart
@@ -1,5 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
+
+enum WeightState {
+  DATA_FETCHED,
+  DATA_NOT_FETCHED,
+}
+
+class firestoreObject {
+  final weight;
+  final occured_at;
+
+  firestoreObject({this.weight, this.occured_at});
+}
 
 class WeightScreen extends StatefulWidget {
   const WeightScreen({Key? key}) : super(key: key);
@@ -9,69 +24,117 @@ class WeightScreen extends StatefulWidget {
 }
 
 class _WeightScreen extends State<WeightScreen> {
+  Map snapshot = {};
+  List<firestoreObject> snapshotDocs = [];
+  List<FlSpot> weightList = [];
+  List<String> dayList = [];
   bool showAvg = false;
+  WeightState _state = WeightState.DATA_NOT_FETCHED;
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Stack(
-          children: <Widget>[
-            Container(
-              decoration: const BoxDecoration(
-                color: Colors.lightBlue,
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(
-                    right: 18.0, left: 12.0, top: 36, bottom: 12),
-                child: AspectRatio(
-                  aspectRatio: 1.50,
-                  child: LineChart(
-                    showAvg ? avgData() : mainData(),
-                  ),
-                ),
+    return _content();
+  }
+
+  Widget _content() {
+    if (_state == WeightState.DATA_FETCHED)
+      return _contentFeched();
+    else
+      return _contentNotFetched();
+  }
+
+  Widget _contentFeched() {
+    return Column(children: [
+      LineChartWidget(),
+      const SizedBox(height: 10),
+      Flexible(
+        child: ListView.builder(
+            itemCount: snapshotDocs.length,
+            itemBuilder: (_, index) {
+              firestoreObject s = snapshotDocs[index];
+              return Card(
+                margin: const EdgeInsets.all(5.0),
+                child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    width: 300,
+                    height: 100,
+                    child: Column(children: [
+                      Text(
+                        "${s.occured_at}",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      Text(
+                        "${s.weight}(kg)",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ])),
+              );
+            }),
+      )
+    ]);
+  }
+
+  Widget _contentNotFetched() {
+    return ListView(children: [
+      LineChartWidget(),
+      const SizedBox(height: 50),
+      Container(
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.blue, width: 5),
+              borderRadius: BorderRadius.circular(10)),
+          child: IconButton(
+            onPressed: () async {
+              DateTime now = DateTime.now();
+
+              final snapshot = await FirebaseFirestore.instance
+                  .collection('weight')
+                  .doc("${FirebaseAuth.instance.currentUser?.email}")
+                  .collection("${now.year}年${now.month}月")
+                  .get();
+
+              snapshot.docs.forEach((element) {
+                var firestoreobj = firestoreObject(
+                    weight: element["weight"],
+                    occured_at: element["occured_at"]);
+                snapshotDocs.add(firestoreobj);
+              });
+              snapshotDocs.sort((a, b) => a.occured_at.compareTo(b.occured_at));
+              snapshotDocs.asMap().forEach((int i, firestoreObject value) {
+                int index = i;
+                final w = value.weight - 45.0;
+                weightList.add(FlSpot(index.toDouble(), w / 5));
+                dayList.add(value.occured_at.toString().substring(8));
+              });
+              setState(() {
+                _state = WeightState.DATA_FETCHED;
+              });
+            },
+            icon: Icon(Icons.get_app),
+            iconSize: 50,
+          ))
+    ]);
+  }
+
+  Widget LineChartWidget() {
+    return Stack(
+      children: <Widget>[
+        Container(
+          decoration: const BoxDecoration(
+            color: Colors.lightBlue,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(
+                right: 18.0, left: 12.0, top: 36, bottom: 0),
+            child: AspectRatio(
+              aspectRatio: 1.50,
+              child: LineChart(
+                mainData(),
               ),
             ),
-            SizedBox(
-              width: 140,
-              height: 30,
-              child: TextButton(
-                onPressed: () {
-                  setState(() {
-                    showAvg = !showAvg;
-                  });
-                },
-                child: Text(
-                  'avg',
-                  style: TextStyle(
-                      fontSize: 12,
-                      color: showAvg
-                          ? Colors.white.withOpacity(0.5)
-                          : Colors.white),
-                ),
-              ),
-            ),
-          ],
-        ),
-        Card(
-          margin: const EdgeInsets.all(5.0),
-          child: Container(
-              margin: const EdgeInsets.all(10.0),
-              width: 300,
-              height: 100,
-              child: Column(children: [
-                const Text(
-                  '12:00~12:15',
-                  style: TextStyle(fontSize: 30),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                const Text(
-                  "0.8 km",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ])),
+          ),
         ),
       ],
     );
@@ -85,14 +148,23 @@ class _WeightScreen extends State<WeightScreen> {
     );
     Widget text;
     switch (value.toInt()) {
-      case 2:
-        text = const Text('MAR', style: style);
-        break;
       case 5:
-        text = const Text('JUN', style: style);
+        text = const Text('5', style: style);
         break;
-      case 8:
-        text = const Text('SEP', style: style);
+      case 10:
+        text = const Text('10', style: style);
+        break;
+      case 15:
+        text = const Text('15', style: style);
+        break;
+      case 20:
+        text = const Text('20', style: style);
+        break;
+      case 25:
+        text = const Text('25', style: style);
+        break;
+      case 30:
+        text = const Text('30', style: style);
         break;
       default:
         text = const Text('', style: style);
@@ -193,102 +265,13 @@ class _WeightScreen extends State<WeightScreen> {
               top: BorderSide(color: Color(0xff37434d), width: 1),
               bottom: BorderSide(color: Color(0xff37434d), width: 1))),
       minX: 0,
-      maxX: 11,
+      maxX: 31,
       minY: 0,
       maxY: 6,
       lineBarsData: [
         LineChartBarData(
           color: Colors.white,
-          spots: const [
-            FlSpot(0, 3),
-            FlSpot(2.6, 2),
-            FlSpot(4.9, 5),
-            FlSpot(6.8, 3.1),
-            FlSpot(8, 4),
-            FlSpot(9.5, 3),
-            FlSpot(11, 4),
-          ],
-          barWidth: 5,
-          isStrokeCapRound: true,
-          dotData: FlDotData(
-            show: false,
-          ),
-          belowBarData: BarAreaData(
-            show: false,
-          ),
-        ),
-      ],
-    );
-  }
-
-  LineChartData avgData() {
-    return LineChartData(
-      lineTouchData: LineTouchData(enabled: false),
-      gridData: FlGridData(
-        show: true,
-        drawVerticalLine: false,
-        drawHorizontalLine: true,
-        verticalInterval: 1,
-        horizontalInterval: 1,
-        getDrawingVerticalLine: (value) {
-          return FlLine(
-            color: const Color(0xff37434d),
-            strokeWidth: 1,
-          );
-        },
-        getDrawingHorizontalLine: (value) {
-          return FlLine(
-            color: const Color(0xff37434d),
-            strokeWidth: 1,
-          );
-        },
-      ),
-      titlesData: FlTitlesData(
-        show: true,
-        bottomTitles: AxisTitles(
-          sideTitles: SideTitles(
-            showTitles: true,
-            reservedSize: 30,
-            getTitlesWidget: bottomTitleWidgets,
-            interval: 1,
-          ),
-        ),
-        leftTitles: AxisTitles(
-          sideTitles: SideTitles(
-            showTitles: true,
-            getTitlesWidget: leftTitleWidgets,
-            reservedSize: 42,
-            interval: 1,
-          ),
-        ),
-        topTitles: AxisTitles(
-          sideTitles: SideTitles(showTitles: false),
-        ),
-        rightTitles: AxisTitles(
-          sideTitles: SideTitles(showTitles: false),
-        ),
-      ),
-      borderData: FlBorderData(
-          show: true,
-          border: const Border(
-              top: BorderSide(color: Color(0xff37434d), width: 1),
-              bottom: BorderSide(color: Color(0xff37434d), width: 1))),
-      minX: 0,
-      maxX: 11,
-      minY: 0,
-      maxY: 6,
-      lineBarsData: [
-        LineChartBarData(
-          color: const Color(0xff7fffd4),
-          spots: const [
-            FlSpot(0, 3.44),
-            FlSpot(2.6, 3.44),
-            FlSpot(4.9, 3.44),
-            FlSpot(6.8, 3.44),
-            FlSpot(8, 3.44),
-            FlSpot(9.5, 3.44),
-            FlSpot(11, 3.44),
-          ],
+          spots: weightList,
           barWidth: 5,
           isStrokeCapRound: true,
           dotData: FlDotData(
